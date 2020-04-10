f0a73234635da9426bdc07a077b5016ffc51668e
Alejandro <alesan.ca3@gmail.com>
2020-04-10 12:00:13 +0100

    New App file
    Created header, maincontent and footer. Import from other files

---
+++ first_app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header className="navbar">
+            <h1> Este es mi header usando React</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ first_app/src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainContent(){
+    return (
+        <div className="MainContent">
+            <h2> Este es mi contenido principal</h2>
+        </div>
+    )
+}
+
+export default MainContent;
