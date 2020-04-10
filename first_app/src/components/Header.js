5cc2ab5995a1671f542256fdb30768c69fe508fb
Alejandro <alesan.ca3@gmail.com>
2020-04-10 12:09:26 +0100

    Remember coment about className

--- first_app/src/components/Header.js
+++ first_app/src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function Header(){
     return(
+        //ClassName no sirve en componentes
         <header className="navbar">
             <h1> Este es mi header usando React</h1>
         </header>
