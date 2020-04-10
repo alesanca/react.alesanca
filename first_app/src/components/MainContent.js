68a8beaa9c570a68ea4c05af34aac91ea233f0d5
Alejandro <alesan.ca3@gmail.com>
2020-04-10 19:28:26 +0100

    Update 10/04/20 afternoon
    Some changes testing const and other functions.

--- first_app/src/components/MainContent.js
+++ first_app/src/components/MainContent.js
@@ -1,9 +1,38 @@
 import React from "react";
 
 function MainContent(){
+
+    //Aquí declaramos las variables que vamos a utilizar
+    const content = "contenido";
+    const principal = "principal";
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
+    //Comprobamos la hora del día y lo que devolver
+
+    if(hours < 12){
+        timeOfDay = "Good morning";
+    } else if (hours >= 12 && hours < 20) {
+        timeOfDay = "Good afternoon";
+    }else{
+        timeOfDay = "Good night";
+    }
+
+    //Si queremos usar puro JS basta con ponerlo entre {}
+
+    const styles = {
+        color: '#FF2D00',
+        fontSize: '24px'
+    }
+
     return (
+        //Esto es JSX, compila HTML
+        //Otra forma de poner el contenido de debajo es:
+        // <h2> Este es mi {`${content} ${principal}`}
         <div className="MainContent">
-            <h2> Este es mi contenido principal</h2>
+            <h2 style={styles}> Este es mi {content + " " + principal}</h2>
+            <h2> {`${timeOfDay}`} </h2>
         </div>
     )
 }
